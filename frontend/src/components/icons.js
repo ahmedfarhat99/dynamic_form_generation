41184354acc2985eb7ab7bc899a79755a32b9c45
Ahmed Farhat <68994170+ahmedfarhat99@users.noreply.github.com>
2023-12-18 03:25:24 +0100

    master

--- frontend/src/components/icons.js
+++ frontend/src/components/icons.js
@@ -1,9 +1,11 @@
 import { FaExternalLinkAlt, FaRegTrashAlt, FaUserCircle } from "react-icons/fa";
+import { HiPlus } from "react-icons/hi";
 
 const icons = {
   openExternal: <FaExternalLinkAlt />,
   delete: <FaRegTrashAlt />,
   defaultUser: <FaUserCircle />,
+  add: <HiPlus />,
 };
 
 export default icons;
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,7 +1,15 @@
+import icons from "../components/icons";
+
 const Home = () => {
   return (
     <div className="home-container">
-      <h1>Home</h1>
+      <div className="general">
+        <h1>Home</h1>
+        <button className="btn text-success border-success">
+          <span className="icon">{icons.add}</span>
+        </button>
+      </div>
+      <form></form>
     </div>
   );
 };
